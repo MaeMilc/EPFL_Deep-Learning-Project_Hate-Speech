69925593a7a19d22c8f4f06dfec34f9abaec5182
Moritz Widmer <moritz.widmer@scs.ch>
2024-05-23 15:26:06 +0200

    renaming of file, added path to few files

--- arabert_arabic_eval.py
+++ arabert_arabic_eval.py
@@ -7,47 +7,15 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 import torchvision
 import requests
-from torcheval.metrics.functional import multiclass_f1_score
-from transformers import RobertaTokenizer, RobertaForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import os
-
-# Defining a new classification layer for Roberta such as to improve our results
-# Please note that our code is an adapted version of the transformers Roberta Classifier as in https://github.com/huggingface/transformers/blob/84ea427f460ffc8d2ddc08a341ccda076c24fc1f/src/transformers/models/roberta/modeling_roberta.py
-class CustomClassificationHead(nn.Module):
-
-    def __init__(self, config):
-        super().__init__()
-        self.dense1 = nn.Linear(config.hidden_size, config.hidden_size)
-        self.dense2 = nn.Linear(config.hidden_size, config.hidden_size)
-        self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
-        classifier_dropout = (
-            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
-        )
-        self.dropout = nn.Dropout(classifier_dropout)
-        self.normalize = nn.LayerNorm(config.hidden_size, eps=config.layer_norm_eps)
-
-    def forward(self, features, **kwargs):
-        x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
-        x = self.dropout(x)
-        residual = x
-        x = self.dense1(x)
-        x = torch.tanh(x)
-        x = self.normalize(x + residual)
-        x = self.dropout(x)
-        residual = x
-        x = self.dense2(x)
-        x = torch.tanh(x)
-        x = self.normalize(x + residual)
-        x = self.dropout(x)
-        x = self.out_proj(x)
-        return x
-
 
 # Don't forget to cite the authors for using their dataset for our project.
 # Load dataset for training into a pandas dataframe
-datasetTrainFilename = "OSACT2022-sharedTask-train-en.csv"
+datasetTrainFilename = "arabic-data/OSACT2022-sharedTask-train.csv"
 datasetTrainPath = os.path.join(os.getcwd(), datasetTrainFilename)
 datasetTrainInDataframe = pd.read_csv(datasetTrainPath, names=['ID', 'Text', 'OFFOrNot', 'HSOrNot', 'VLGOrNot', 'VIOOrNot'], header=None)
+datasetTrainInDataframe = datasetTrainInDataframe.iloc[1:]
 
 # Process labels to binary format
 accumulatorList = []
@@ -61,18 +29,18 @@ for index, row in datasetTrainInDataframe.iterrows():
 datasetTrainInDataframe.insert(6, "BinLabel", accumulatorList)
 datasetTrain = datasetTrainInDataframe[["Text", "BinLabel"]]
 
-
 # Load test dataset into a pandas dataframe
-datasetTestFilename = "OSACT2022-sharedTask-test-tweets-en.csv"
+datasetTestFilename = "arabic-data/OSACT2022-sharedTask-test-tweets.csv"
 datasetTestPath = os.path.join(os.getcwd(), datasetTestFilename)
 datasetTestInDataframe = pd.read_csv(datasetTestPath, names=['ID', 'Text'], header=None)
+datasetTestInDataframe = datasetTestInDataframe.iloc[1:]
 
 # Load labels which are separately stored
-labelHSFilename = "OSACT2022-sharedTask-test-HS-gold-labels.csv"
+labelHSFilename = "arabic-data/OSACT2022-sharedTask-test-HS-gold-labels.csv"
 labelHSPath = os.path.join(os.getcwd(), labelHSFilename)
 labelHSDataframe = pd.read_csv(labelHSPath, names=['HSOrNot'], header=None)
 datasetTestInDataframe.insert(2, "HSOrNot", list(labelHSDataframe['HSOrNot']))
-labelOFFFilename = "OSACT2022-sharedTask-test-OFF-gold-labels.csv"
+labelOFFFilename = "arabic-data/OSACT2022-sharedTask-test-OFF-gold-labels.csv"
 labelOFFPath = os.path.join(os.getcwd(), labelOFFFilename)
 labelOFFDataframe = pd.read_csv(labelOFFPath, names=['OFFOrNot'], header=None)
 datasetTestInDataframe.insert(3, "OFFOrNot", list(labelOFFDataframe['OFFOrNot']))
@@ -90,9 +58,10 @@ datasetTestInDataframe.insert(4, "BinLabel", accumulatorList)
 datasetTest = datasetTestInDataframe[["Text", "BinLabel"]]
 
 # Load dev dataset into a pandas dataframe
-datasetDevFilename = "OSACT2022-sharedTask-dev-en.csv"
+datasetDevFilename = "arabic-data/OSACT2022-sharedTask-dev.csv"
 datasetDevPath = os.path.join(os.getcwd(), datasetDevFilename)
 datasetDevInDataframe = pd.read_csv(datasetDevPath, names=['ID', 'Text', 'OFFOrNot', 'HSOrNot', 'VLGOrNot', 'VIOOrNot'], header=None)
+datasetDevInDataframe = datasetDevInDataframe.iloc[1:]
 
 # Process labels to binary format
 accumulatorList = []
@@ -107,15 +76,10 @@ datasetDevInDataframe.insert(6, "BinLabel", accumulatorList)
 datasetDev = datasetDevInDataframe[["Text", "BinLabel"]]
 
 # Next step would be tokenization of the text as input for our model.
-modelFilename = "custom_classifier_trained_engl.pth"
-modelPath = os.path.join(os.getcwd(), modelFilename)
-model = RobertaForSequenceClassification.from_pretrained('roberta-base')
-
-# Replace the classifier with the custom one
-model.classifier = CustomClassificationHead(model.config)
-
-model.load_state_dict(torch.load(modelPath))
-tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
+# Loading the tokenizer and model
+model_name = "aubmindlab/bert-large-arabertv02-twitter"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
 
 # Define dataset class
 class CustomDataset(Dataset):
@@ -153,41 +117,48 @@ class CustomDataset(Dataset):
 train_texts = datasetTrain["Text"].tolist()
 train_labels = datasetTrain["BinLabel"].tolist()
 
-# Define training dataset and data loader
+
 train_dataset = CustomDataset(train_texts, train_labels, tokenizer, max_length=128)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 
-# Freeze pre-trained model layers
-for param in model.roberta.parameters():
-   param.requires_grad = False
 
-# Define loss function and optimizer
-criterion = nn.CrossEntropyLoss()
+# Model training function
+def train_model(model, train_loader, optimizer, criterion, epochs=35):
+    model.train()
+    for epoch in range(epochs):
+        total_loss = 0
+        for batch in train_loader:
+            input_ids = batch['input_ids']
+            attention_mask = batch['attention_mask']
+            labels = batch['labels']
+            
+            optimizer.zero_grad()
+            outputs = model(input_ids, attention_mask=attention_mask)
+            loss = criterion(outputs.logits, labels)
+            loss.backward()
+            optimizer.step()
+            total_loss += loss.item()
+            print(f'Epoch {epoch+1}: Loss {total_loss/len(train_loader)}')
+
+
+        
+# Training setup
 optimizer = optim.AdamW(model.parameters(), lr=2e-5)
+criterion = nn.CrossEntropyLoss()
 
-# train loader contains 1029 batches
+# Freeze pre-trained model layers
+for param in model.bert.parameters():
+    param.requires_grad = False
 
-# Training loop
-num_epochs = 35
 
-model.train()
-for epoch in range(num_epochs):
-    count = 0
-    for batch in train_loader:
-        input_ids = batch["input_ids"]
-        attention_mask = batch["attention_mask"]
-        labels = batch["labels"]
+# Run this cell to train the model
+train_model(model, train_loader, optimizer, criterion)
 
-        optimizer.zero_grad()
-        outputs = model(input_ids, attention_mask=attention_mask)
-        loss = criterion(outputs.logits, labels)
-        loss.backward()
-        optimizer.step()
-        print("Epoch: ", epoch, "  Count: ", count)
-        count += 1
 
-# Save fineturned model
-torch.save(model.state_dict(), "custom_classifier_trained_engl_arab.pth")
+# Save the model and weights
+# model.save_pretrained('arabert-pretrained')
+torch.save(model.state_dict(), "arabert-weights.pth")
+
 
 # Load Datasets
 val_texts = datasetTest["Text"].tolist()
@@ -197,21 +168,10 @@ val_labels = datasetTest["BinLabel"].tolist()
 val_dataset = CustomDataset(val_texts, val_labels, tokenizer, max_length=128)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 
-# Evaluation loop
+
 model.eval()
 val_loss = 0.0
 val_correct = 0
-counter1 = []
-
-predictedLabels = []
-trueLabels = []
-
-
-for batch in val_loader:
-    counter1.append("0")
-
-
-print(len(counter1))
 
 counter = 0
 
@@ -231,19 +191,11 @@ with torch.no_grad():
         print("counter", counter)
         counter = counter + 1
 
-        # Update for f1 score
-        predictedLabels = predictedLabels + predicted.tolist()
-        trueLabels = trueLabels + labels.tolist()
-
 # Calculate average validation loss
 avg_val_loss = val_loss / len(val_loader.dataset)
 
 # Calculate validation accuracy
 val_accuracy = val_correct / len(val_loader.dataset)
 
-# Get the f1 score
-f1_score = multiclass_f1_score(torch.Tensor(predictedLabels), torch.Tensor(trueLabels), num_classes=2)
-
 print(f"Validation Loss: {avg_val_loss:.4f}")
 print(f"Validation Accuracy: {val_accuracy:.4f}")
-print(f"Validation F1-Score: {f1_score:.4f}")
